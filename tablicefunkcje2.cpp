5718bbb83cf39b2ecf1046bd6a6b27f6f5d5ae61
PS-TS <115213337+PS-TS@users.noreply.github.com>
2022-11-14 08:43:06 +0100

    Add files via upload

---
+++ tablicefunkcje2.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int parz(int tabl[]) {
+    int wynik = 0;
+    for (int i = 0; i < 20; i++){
+        if(tabl[i]%2==0)
+        wynik ++;
+    }
+    return wynik;
+}
+
+int main()
+{
+    int tab[20];
+    srand(time(NULL));
+    for (int i = 0; i < 20; i++){
+        tab[i] = rand() % 101;
+    }
+    cout<<parz(tab);
+}
+
