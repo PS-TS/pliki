4e7e373bf6e10d19f371cdf27cac4c365044618e
PS-TS <115213337+PS-TS@users.noreply.github.com>
2023-02-22 11:46:37 +0100

    sprzatanie

---
+++ semestr1/egzamin programowanie/egz_30_01_23/zad3.cpp
@@ -0,0 +1,31 @@
+// nie wiem czy dobrze zrozumiałam polecenie
+
+
+#include <iostream>
+using namespace std;
+
+int funkcja(int n);
+
+int main()
+{
+    cout << funkcja(5);
+}
+
+int funkcja(int n)
+{
+    int liczba = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (liczba >= 0)
+        {
+            liczba += 3;
+            liczba *= -1;
+        }
+        else
+        {
+            liczba -= 3;
+            liczba *= -1;
+        }
+    }
+    return liczba;
+}
