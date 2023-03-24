ee3a031d8038ffbbe7d3cb9eb5a2ddb5478236b2
RymszewiczAnna <115213228+RymszewiczAnna@users.noreply.github.com>
2023-03-24 14:59:49 +0100

    Update zadanie 7.py

--- semestr2/wizualizacjaweiss/zadanie 7.py
+++ semestr2/wizualizacjaweiss/zadanie 7.py
@@ -1,16 +1,13 @@
 import numpy as np
 
-
 def func(n):
     macierz = np.zeros((n,n))
-    macierz = np.diag([2 for i in range(n)])
+    for i in range(n):
+        for j in range(n):
 
-    macierz[:-1, 1:] = np.diag([2 * 2 for i in range(n - 1)])
-    macierz[1:,:-1] = np.diag([2 * 2 for i in range(n - 1)])
-    macierz[:-2, 2:] = np.diag([2 * 3 for i in range(n - 2)])
-    macierz[2:, :-2] = np.diag([2 * 3 for i in range(n - 2)])
+            macierz[i,j] = 2*(abs(i-j)+1)
 
     print(macierz)
 
 
-func(3)
+func(8)
