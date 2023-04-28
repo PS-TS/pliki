76ddf0b4d80df4285662e8e07cf91e42597b6baa
RymszewiczAnna <115213228+RymszewiczAnna@users.noreply.github.com>
2023-04-28 15:26:15 +0200

    Update 6.py

--- semestr2/wizualizacjaweiss/ćwiczenia10/6.py
+++ semestr2/wizualizacjaweiss/ćwiczenia10/6.py
@@ -8,3 +8,31 @@ plt.ylabel('Suma')
 plt.title("Suma urodzonych dzieci w danym roku: ")
 c.plot.bar(color=['black'])
 plt.show()
+
+
+#rymszewicz
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+
+xlsx = pd.ExcelFile('imiona.xlsx')
+df = pd.read_excel(xlsx, header=0)
+a = df.groupby(['Plec']).agg({'Liczba':[sum]})
+a.plot.bar()
+b1 = df[df['Plec'] == 'K']
+b1dane = b1.groupby(['Rok']).agg({'Liczba':[sum]})
+
+b1dane.plot.bar()
+c = df.groupby(['Rok']).agg({'Liczba': ['sum']})
+c.plot.bar(color=['black'])
+
+
+
+
+
+plt.xlabel('Rok')
+plt.ylabel('Suma')
+plt.title("Suma urodzonych dzieci w danym roku: ")
+
+plt.show()
