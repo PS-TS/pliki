1d74d84e2dbae29ef422372084954fbe2372194b
Michał Turczyn <169391@student.uwm.edu.pl>
2023-10-18 23:38:15 +0200

    algorytmy_lab1

---
+++ semestr3/algorytmy_cwiczenia/lab1-04-10/lab1/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            przycisk = new Button();
+            numericUpDown1 = new NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+            SuspendLayout();
+            // 
+            // przycisk
+            // 
+            przycisk.Location = new Point(464, 273);
+            przycisk.Name = "przycisk";
+            przycisk.Size = new Size(221, 93);
+            przycisk.TabIndex = 0;
+            przycisk.Text = "uruchom";
+            przycisk.UseVisualStyleBackColor = true;
+            przycisk.Click += button1_Click;
+            // 
+            // numericUpDown1
+            // 
+            numericUpDown1.Location = new Point(464, 217);
+            numericUpDown1.Name = "numericUpDown1";
+            numericUpDown1.Size = new Size(221, 27);
+            numericUpDown1.TabIndex = 1;
+            numericUpDown1.ValueChanged += numericUpDown1_ValueChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1257, 450);
+            Controls.Add(numericUpDown1);
+            Controls.Add(przycisk);
+            Name = "Form1";
+            Text = "Form1";
+            Load += Form1_Load;
+            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button przycisk;
+        private NumericUpDown numericUpDown1;
+    }
+}
