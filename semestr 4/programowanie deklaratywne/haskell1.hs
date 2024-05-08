fb7bdd054d79578ca150da07c92836dca4d24595
PS-TS <115213337+PS-TS@users.noreply.github.com>
2024-05-08 11:47:25 +0200

    Create haskell1.hs

---
+++ semestr 4/programowanie deklaratywne/haskell1.hs
@@ -0,0 +1,52 @@
+kwadrat::Float->Float
+kwadrat x=x*x
+pkolo r=pi*r*r
+obkuli r=a*pi*sz where 
+		a=4/3 
+		sz=r*r*r
+abs::Float->Float
+abs x=if x<0 then (-x) else x
+min:: Integer->Integer->Integer
+min x y=if x>y then y else x
+
+abs2::Float->Float
+abs2 x
+	|x<0 = (-x)
+	|otherwise = x
+	
+sgn::Float->Float
+sgn x=if x>0 then 1 else if x<0 then (-1) else 0
+
+sgn2::Float->Float
+sgn2 x
+	|x>0 = 1
+	|x<0 = (-1)
+	|otherwise = 0
+	
+albo_albo1::(Bool,Bool)->Bool
+albo_albo1 (p,q)= case (p,q) of (True,True)->False
+				(True,False)->True
+				(False,True)->True
+				(False,False)->False
+albo_albo2::Bool->Bool->Bool
+albo_albo2 p q=if p==q then False else True
+
+albo_albo3::Bool->Bool->Bool
+albo_albo3 p q
+	|p==q=False
+	|otherwise=True
+albo_albo4::Bool->Bool->Bool
+albo_albo4 x y= x&&not y||not x&&y
+
+imp::Bool->Bool->Bool
+imp p q=if p==True&&q==False then False else True
+
+test::Bool->Bool->Bool
+test x y=if x==y then True else False
+
+test1::Float->Float->Float
+test1 x y
+	|x>y = 1
+	|x==y = 0
+	|x<y = (-1)
+ 
