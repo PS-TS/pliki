c5cb0e29a0653697ce32a1ee7a268978be9a9edb
PS-TS <115213337+PS-TS@users.noreply.github.com>
2025-06-03 22:59:50 +0200

    Update cw1.py

--- MIW/cw1.py
+++ MIW/cw1.py
@@ -3,19 +3,27 @@ import numpy as np
 if __name__ == '__main__':
     file_path = 'values.txt'
     array = np.loadtxt(file_path, dtype=int)
-    print(array)
     decisions = array[:, 6]
+    newarray = []
+    seen = set()  # Do przechowywania unikalnych wpisów
 
     for z in range(len(array)):
-        for y in range (6):
-            test = True
-            wartosc = array[z,y]
+        for y in range(6):
+            wartosc = array[z, y]
             decyzja = decisions[z]
+            test = True
+
             for x in range(len(array)):
-                if array[x,y] == wartosc and decisions[x] != decyzja:
+                if array[x, y] == wartosc and decisions[x] != decyzja:
                     test = False
                     break
-            if test:
-                print(f"Rzad {z+1} Kolumna {y+1} = {wartosc} Wynik {decyzja}")
 
-// dodać ignorowane wartości oraz drugi stopień
+            if test:
+                entry = (y + 1, int(wartosc), int(decyzja))
+                if entry not in seen:
+                    print(f"Rząd {z + 1} Kolumna {y + 1} = {wartosc} Wynik {decyzja}")
+                    newarray.append(entry)
+                    seen.add(entry)
+
+    print(newarray)
+
